f8c7dfaa912bd6130f0734bd7e22154a3109fc17
SamYuan1990 <yy19902439@126.com>
2020-03-30 20:34:26 +0800

    update Hospital sh

--- blockchaine2e/myscripHospital.sh
+++ blockchaine2e/myscripHospital.sh
@@ -114,7 +114,7 @@ ARGS='{"Args":[]}'
 instantiateChaincodeMyCC 0 1
 #instantiateChaincodeMyCC 0 2
 sleep 60
-ARGS='{"Args":["UpdateLocation","A"]}'
+ARGS='{"Args":["UpdateLocation","[\"M0001\",\"M0002\"]"]}'
 chaincodeInvokeMyCC 0 1 0 2
 ARGS='{"Args":["getLocations"]}'
 chaincodeInvokeMyCC 0 1 0 2
