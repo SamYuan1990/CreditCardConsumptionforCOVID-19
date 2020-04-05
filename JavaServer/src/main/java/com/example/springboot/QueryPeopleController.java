75e41bf3f8169f877933b63155ffc4219794787a
SamYuan1990 <yy19902439@126.com>
2020-04-06 00:15:48 +0800

    add new feture

---
+++ JavaServer/src/main/java/com/example/springboot/QueryPeopleController.java
@@ -0,0 +1,32 @@
+package com.example.springboot;
+
+import com.example.springboot.util.utils;
+import com.google.gson.Gson;
+import org.hyperledger.fabric.sdk.Channel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletRequest;
+
+@RestController
+public class QueryPeopleController {
+
+	@RequestMapping("/getPeople")
+	public String index(ServletRequest req) {
+		Channel mychannel = null;
+		try {
+			mychannel = utils.mychannelPool.borrowObject();
+
+
+			String payload = utils.Query(mychannel,utils.HospitalCC,"getPatientInfo",req.getParameter("Credit_card"));
+			//Gson gson = new Gson();
+			//String[] object = gson.fromJson(payload, String[].class);
+			utils.mychannelPool.returnObject(mychannel);
+
+			return payload;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
